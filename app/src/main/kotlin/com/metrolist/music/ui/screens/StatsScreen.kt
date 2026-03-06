55f9c0574d0c6a471e87cc74b65b49ce2b2d6da8
Finley Webber <webberfinley@gmail.com>
2026-03-06 21:01:56 +1100

    Merged with Time Transfer

--- app/src/main/kotlin/com/metrolist/music/ui/screens/StatsScreen.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/StatsScreen.kt
@@ -39,15 +39,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -484,7 +481,6 @@ fun StatsScreen(
                         }
                     }
                 }
-            }
             }
             }
 
