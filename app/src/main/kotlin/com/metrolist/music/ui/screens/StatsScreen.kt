9fe4dbc0b724fab7f04e434f29baa92ddf052eb0
Finley Webber <webberfinley@gmail.com>
2026-03-12 20:24:42 +1100

    Resolved Conflicts with new update + made Searching compatible with new 'Play All' button in the songs column

--- app/src/main/kotlin/com/metrolist/music/ui/screens/StatsScreen.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/StatsScreen.kt
@@ -39,7 +39,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
